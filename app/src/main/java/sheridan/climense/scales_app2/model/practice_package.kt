7e0cba697808b98e8951e91e4a53b8a42e2d1119
Seth Climenhaga <climense@sheridanCollege.ca>
2020-10-28 08:19:33 -0400

    Added practice_package data class

---
+++ app/src/main/java/sheridan/climense/scales_app2/model/practice_package.kt
@@ -0,0 +1,8 @@
+package sheridan.climense.scales_app2.model
+/**
+ *scales_app2
+createdbyseth*
+studentID:991599894
+ *on2020-10-28
+ */
+data class practice_package (val routine_name : String, val practice_array : Array<String>)
